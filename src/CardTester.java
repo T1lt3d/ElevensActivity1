9b3152fcc8d5f7f6aff874e6b9e71014560b54b7
BT_1N3_34 <yibarra0090@bths.edu>
2019-02-14 14:42:07 -0500

    123

--- src/CardTester.java
+++ src/CardTester.java
@@ -2,4 +2,9 @@
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+    public static void main(String [] args){
+        Card card1 = new Card("10","hearts", 10 );
+        System.out.print(card1);
+    }
+
 }
