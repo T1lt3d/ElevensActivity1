5eb2fc1cded83c78324d01ee12837724813e5837
f <g>
2019-02-14 14:19:20 -0500

    Initial Card Class

--- src/Card.java
+++ src/Card.java
@@ -32,7 +32,9 @@ public class Card {
      *                  containing the point value of the card
      */
     public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		this.rank = cardRank;
+		this.suit = cardSuit;
+		this.pointValue = cardPointValue;
     }
 
 
@@ -41,7 +43,7 @@ public class Card {
      * @return this <code>Card's</code> suit.
      */
     public String suit() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return suit;
 
     }
 
@@ -50,8 +52,7 @@ public class Card {
      * @return this <code>Card's</code> rank.
      */
     public String rank() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+		return rank;
     }
 
     /**
@@ -59,8 +60,7 @@ public class Card {
      * @return this <code>Card's</code> point value.
      */
     public int pointValue() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+		return pointValue;
     }
 
     /** Compare this card with the argument.
